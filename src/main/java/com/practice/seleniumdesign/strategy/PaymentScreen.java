f82c96da4a3dec6048c5708f13b43e91e2e9a2b6
Shank24 <shankkalra@gmail.com>
2020-07-19 08:19:37 +0530

    Adding SDP

---
+++ src/main/java/com/practice/seleniumdesign/strategy/PaymentScreen.java
@@ -0,0 +1,12 @@
+package com.practice.seleniumdesign.strategy;
+
+import org.openqa.selenium.WebDriver;
+
+public class PaymentScreen {
+
+    private WebDriver driver;
+
+    public PaymentScreen(final WebDriver driver){
+        this.driver=driver;
+    }
+}
