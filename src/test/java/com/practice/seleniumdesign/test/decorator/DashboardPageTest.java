c42a1a6aee6c6aab46806977a51e9b9b455b4baa
Shank24 <shankkalra@gmail.com>
2020-07-24 19:42:00 +0530

    Adding Decorator Pattern Code

---
+++ src/test/java/com/practice/seleniumdesign/test/decorator/DashboardPageTest.java
@@ -0,0 +1,34 @@
+package com.practice.seleniumdesign.test.decorator;
+
+import com.practice.seleniumdesign.decorator.DashboardPage;
+import com.practice.seleniumdesign.test.BaseTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.util.function.Consumer;
+
+public class DashboardPageTest extends BaseTest {
+
+    private DashboardPage dashboardPage;
+
+    @BeforeTest
+    public void setDashboardPage(){
+        this.dashboardPage = new DashboardPage(driver);
+    }
+
+    @Test(dataProvider = "getData")
+    public void roleTest(Consumer<DashboardPage> role){
+        this.dashboardPage.goTo();
+        role.accept(this.dashboardPage);
+    }
+
+    @DataProvider
+    public Object[] getData(){
+        return new Object[]{
+                Decorators.guestPage,
+                Decorators.suPage,
+                Decorators.adminPage
+        };
+    }
+}
